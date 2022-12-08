8b67378abce5573a3954af26598d894dbdbf956b
Roman <romannemeth1@gmail.com>
2022-12-08 18:39:09 +0100

    created error boundary

---
+++ toDoApp/frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { ErrorInfo } from "react";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../styles/errorBoundary.module.css";
+import { Props, State } from "../types/others";
+
+class ErrorBoundary extends React.Component<Props, State> {
+    public state: State = {
+        hasError: false,
+    };
+
+    static getDerivedStateFromError(error: Error): State {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        console.error("Uncaught error:", error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className={styles.errorContainer}>
+                    <p>
+                        <FontAwesomeIcon size="sm" icon={faCircleExclamation} />
+                        {` Sorry, something went wrong...`}
+                    </p>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
---
+++ toDoApp/frontend/src/types/others.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children?: ReactNode;
+};
+
+type State = {
+    hasError: boolean;
+};
+
+export type { Props, State };
